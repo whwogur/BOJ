8a0eb0c3b9fb3d2ec9518af2cb104e6808c8bc9b
whwogur <cjh.lux@gmail.com>
2023-03-17 15:27:23 +0900

    230317

--- 5639_이진검색트리.py
+++ 5639_이진검색트리.py
@@ -1,29 +1,34 @@
 # 백준 5639
 import sys
-input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
-num_list = []
 
+# dfs 탐색
+def dfs(start, end):
+
+    # 시작과 끝 값이 역전시 리턴
+    if start > end:
+        return
+
+    temp = end + 1
+
+    # 서브 트리 찾기
+    for i in range(start + 1, end + 1):
+        # 루트 보다 크면 오른쪽 서브 트리
+        if graph[start] < graph[i]:
+            temp = i
+            break
+
+    dfs(start + 1, temp - 1) # 왼쪽 서브 트리 재귀적으로 탐색
+    dfs(temp, end) # 오른쪽 서브 트리 재귀적으로 탐색
+
+    print(graph[start])
+
+
+# 입력이 없을때까지 반복하여 입력을 리스트에 추가한다.
+graph = []
 while True:
     try:
-        num = int(input())
-        num_list.append(num)
+        graph.append(int(sys.stdin.readline()))
     except:
         break
 
-def postOrderTraversal(first, end):
-    if first > end:
-        return
-    mid = end + 1 # 루트보다 큰 값이 존재하지 않을 경우를 대비
-    for i in range(first + 1, end + 1):
-        if num_list[first] < num_list[i]:
-            mid = i
-            break
-
-    postOrderTraversal(first + 1, mid - 1)
-    postOrderTraversal(mid, end)
-    print(num_list[first])
-
-postOrderTraversal(0, len(num_list) - 1)
-
-
+dfs(0, len(graph) - 1)
