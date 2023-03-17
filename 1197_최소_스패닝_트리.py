366d66adc02c56a964ae048e4c581cda663aaee4
whwogur <cjh.lux@gmail.com>
2023-03-17 10:38:32 +0900

    230317

---
+++ 1197_최소_스패닝_트리.py
@@ -0,0 +1,41 @@
+# 1197 최소 스패닝트리(크러스컬 알고리즘)
+import sys
+input = sys.stdin.readline
+
+def find_parent(parent, x):
+    # 루트 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+# 두 원소가 속한 집합 합치기
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+v, e = map(int, input().rstrip().split())
+parent = [0] * (v + 1)
+edges = []
+result = 0
+
+for i in range(1, v+1):
+    parent[i] = i
+
+for _ in range(e):
+    a, b, cost = map(int, input().split())
+    edges.append((cost, a, b)) # 비용순으로 정렬하기위해 첫번쨰원소 비용
+
+edges.sort()
+
+for edge in edges:
+    cost, a, b = edge
+    # 사이클이 발생하지 않는 경우에만 집합에 포함
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += cost
+
+print(result)
