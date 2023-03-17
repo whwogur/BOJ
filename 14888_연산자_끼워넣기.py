7d51c09b26ae2c72d4b66769a635947069de8f1b
whwogur <cjh.lux@gmail.com>
2023-03-17 19:53:36 +0900

    230317

--- 14888_연산자_끼워넣기.py
+++ 14888_연산자_끼워넣기.py
@@ -1,30 +1,30 @@
 # 14888 연산자 끼워넣기
 
-def dfs(i, arr):
+def dfs(i, num):
     global add, sub, mul, div, max_value, min_value
 
     if i == n:
-        max_value = max(max_value, arr)
-        min_value = min(min_value, arr)
+        max_value = max(max_value, num)
+        min_value = min(min_value, num)
     else:
         if add > 0:
             add -= 1
-            dfs(i+1, arr + data[i])
+            dfs(i+1, num + data[i])
             add += 1
 
         if sub > 0:
             sub -= 1
-            dfs(i+1, arr - data[i])
+            dfs(i+1, num - data[i])
             sub += 1
 
         if mul > 0:
             mul -= 1
-            dfs(i+1, arr * data[i])
+            dfs(i+1, num * data[i])
             mul += 1
 
         if div > 0:
             div -= 1
-            dfs(i+1, arr // data[i])
+            dfs(i+1, num // data[i])
             div += 1
 
 n = int(input().rstrip())
