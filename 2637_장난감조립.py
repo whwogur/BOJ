8fa7f2b55b37512dc335acb2b9afd096e11b337f
whwogur <cjh.lux@gmail.com>
2023-03-20 14:15:41 +0900

    230320

--- 2637_장난감조립.py
+++ 2637_장난감조립.py
@@ -6,7 +6,7 @@ input = sys.stdin.readline
 n = int(input().rstrip())
 m = int(input().rstrip())
 adj = [[] for _ in range(n + 1)]
-required = [[0] * (n + 1) for _ in range(n + 1)]
+parts_needed = [[0] * (n + 1) for _ in range(n + 1)]
 q = deque()
 degree = [0] * (n + 1)
 for _ in range(m):
@@ -21,14 +21,14 @@ for i in range(1, n + 1):
 while q:
     now = q.popleft()
     # 현 제품의 다음 단계 번호, 현 제품이 얼마나 필요한지
-    for next, next_required in adj[now]:
+    for next, next_parts_needed in adj[now]:
         # 만약 현 제품이 기본 부품이면
-        if required[now].count(0) == n + 1:
-            required[next][now] += next_required
+        if parts_needed[now].count(0) == n + 1:
+            parts_needed[next][now] += next_parts_needed
         # 현 제품이 중간 제품이면
         else:
             for i in range(1, n + 1):
-                required[next][i] += required[now][i] * next_required
+                parts_needed[next][i] += parts_needed[now][i] * next_parts_needed
 
         # 차수 - 1
         degree[next] -= 1
@@ -36,6 +36,6 @@ while q:
             # 차수 0아면 큐에 넣음
             q.append(next)
 
-for x in enumerate(required[n]):
+for x in enumerate(parts_needed[n]):
     if x[1] > 0:
         print(*x)
