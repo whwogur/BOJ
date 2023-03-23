55b49d6b0626aeff10919cc57454d98808363858
whwogur <cjh.lux@gmail.com>
2023-03-23 15:25:51 +0900

    피보나치수 2

---
+++ 2748.py
@@ -0,0 +1,13 @@
+# 2748 피보나치 수 2
+import sys
+input = sys.stdin.readline
+
+n = int(input().rstrip())
+dp =[[] for _ in range(100)]
+
+dp[0] = 0
+dp[1] = 1
+for i in range(2, n+1):
+    dp[i] = dp[i - 1] + dp[i - 2]
+
+print(dp[n])
