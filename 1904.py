afb1f3c2a8114d2ac10fe12180d16c07caf6c3b4
whwogur <cjh.lux@gmail.com>
2023-03-23 16:03:58 +0900

    01타일

--- 1904.py
+++ 1904.py
@@ -1,14 +1,14 @@
 # 1904 01타일
 import sys
 input = sys.stdin.readline
-
+DIV = 15746
 n = int(input().rstrip())
 
-dp = [[]for _ in range(n + 1)]
+dp = [[]for _ in range(n + 100)]
 
 dp[1] = 1
 dp[2] = 2
 for i in range(3, n + 1):
-    dp[i] = dp[i - 1] + dp[i - 2]
+    dp[i] = (dp[i - 1] + dp[i - 2]) % DIV
 
 print(dp[n])
