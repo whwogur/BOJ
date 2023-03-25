1681beb23b9dbe8789f054b10491427839231c1e
whwogur <cjh.lux@gmail.com>
2023-03-25 17:08:29 +0900

    오름세

--- 3745.py
+++ 3745.py
@@ -1,28 +1,11 @@
 # 3745 오름세
-import sys
-input = sys.stdin.readline
-
-while True:
-    n = input().rstrip()
-    if not n:
-        break
-    n = int(n)
-    arr = list(map(int, input().split()))
-    dp = [0 for _ in range(n)]
-    dp[0] = arr[0]
-    length = 1
-    for x in arr[1:]:
-        idx, start, end = 0, 0, length - 1
-        while start <= end:
-            mid = (start + end) // 2
-
-            if dp[mid] < x:
-                start = mid + 1
-                idx = max(idx, mid + 1)
-            else:
-                end = mid - 1
-        
-        length = max(idx + 1, length)
-        dp[idx] = x
-
-    print(length)
+import bisect
+try:
+    while input():
+        q=[]
+        for i in map(int,input().split()):
+            s=bisect.bisect_left(q,i)
+            if s!=len(q):q[s]=i
+            else:q+=[i]
+        print(len(q))
+except:exit()
