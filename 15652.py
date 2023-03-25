547244929147f46ee0274b3358338bf779ef64ac
whwogur <cjh.lux@gmail.com>
2023-03-25 13:10:37 +0900

    N과 M 4

---
+++ 15652.py
@@ -0,0 +1,15 @@
+# N과 M 4
+n,m= map(int,input().split())
+ 
+s = []
+ 
+def dfs(a):
+    if len(s) == m:
+        print(' '.join(map(str,s)))
+        return
+    
+    for i in range(a,n+1):
+        s.append(i)
+        dfs(i)
+        s.pop()
+dfs(1)
