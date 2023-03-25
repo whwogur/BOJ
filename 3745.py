536f4dcb189a333a5f632f2c733f0d3ba64f209e
whwogur <cjh.lux@gmail.com>
2023-03-25 17:11:18 +0900

    오름세

--- 3745.py
+++ 3745.py
@@ -2,10 +2,10 @@
 import bisect
 try:
     while input():
-        q=[]
+        q = []
         for i in map(int,input().split()):
-            s=bisect.bisect_left(q,i)
-            if s!=len(q):q[s]=i
-            else:q+=[i]
+            s = bisect.bisect_left(q, i)
+            if s != len(q):q[s] = i
+            else: q += [i]
         print(len(q))
 except:exit()
