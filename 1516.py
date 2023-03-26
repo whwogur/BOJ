44882d84a80bebe63878610fbbcfabccd5f2d1cf
whwogur <cjh.lux@gmail.com>
2023-03-26 19:39:51 +0900

    게임 개발

--- 1516.py
+++ 1516.py
@@ -1,35 +1,36 @@
+# 1516 게임개발
 import sys
 from collections import deque
 input = sys.stdin.readline
- 
-N = int(input())
-queue = deque()
-indegree = [0]*(N+1)
-arr = [[0]*(N+1) for _ in range(N+1)]
-time = [0]*(N+1)
- 
-for i in range(1, N+1):
+
+n = int(input())
+q = deque()
+indegree = [0]*(n+1)
+arr = [[0]*(n+1) for _ in range(n+1)]
+time = [0]*(n+1)
+
+for i in range(1, n+1):
     _input = list(map(int, input().split()))
     time[i] = _input[0]
     for x in _input[1:-1]:
         arr[i][x] = 1
         indegree[i] += 1
- 
-for i in range(1, N+1):
+
+for i in range(1, n+1):
     if indegree[i] == 0:
-        queue.append(i)
- 
-while queue:
-    x = queue.popleft()
+        q.append(i)
+
+while q:
+    x = q.popleft()
     t = 0
-    for i in range(1, N+1):
+    for i in range(1, n+1):
         if arr[i][x] == 1:
             indegree[i] -= 1
             if indegree[i] == 0:
-                queue.append(i)
+                q.append(i)
         if arr[x][i] == 1:
             t = max(time[i], t)
     time[x] += t
- 
+
 for i in time[1:]:
     print(i)
