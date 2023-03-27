ec12baf082e8d08691aff193b378df8b8e676b7a
whwogur <cjh.lux@gmail.com>
2023-03-27 10:39:23 +0900

    LCS 2

--- 9252.py
+++ 9252.py
@@ -4,11 +4,11 @@ input = sys.stdin.readline
 
 word1 = [''] + list(input().strip())
 word2 = [''] + list(input().strip())
-l_1, l_2 = len(word1), len(word2)
-dp = [[''] * l_2 for i in range(l_1)]
+len_1, len_2 = len(word1), len(word2)
+dp = [[''] * len_2 for _ in range(len_1)]
 
-for i in range(l_1):
-    for j in range(l_2):
+for i in range(len_1):
+    for j in range(len_2):
         if word1[i] == word2[j]:
             dp[i][j] = dp[i - 1][j - 1] + word1[i]
         else:
@@ -17,5 +17,5 @@ for i in range(l_1):
             else:
                 dp[i][j] = dp[i][j - 1]
 
-print(len(dp[l_1 - 1][l_2 - 1]))
-print(dp[l_1 - 1][l_2 - 1])
+print(len(dp[len_1 - 1][len_2 - 1]))
+print(dp[len_1 - 1][len_2 - 1])
