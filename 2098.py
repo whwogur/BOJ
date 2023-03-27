033988d2590548100e30e26b4d782da9f334ada9
whwogur <cjh.lux@gmail.com>
2023-03-27 12:44:17 +0900

    외판원 순회

--- 2098.py
+++ 2098.py
@@ -3,28 +3,30 @@ import sys
 input = sys.stdin.readline
 INF = int(1e9)
 # dp[cur][visited] = min(dp[cur][visited], dp[next][visited | (1 << next)] + graph[cur][next])
-n = int(input().rstrip())
-dp = [[INF] * (1<<n)for _ in range(n)]
 
-def dfs(x, visited):
-    if visited == (1<<n) - 1:
-        if graph[x][0]:
-            return graph[x][0]
-        else:
+def recursion(x, visited):
+    if visited == (1 << n)-1:
+        if graph[x][0] == 0:
             return INF
+        dp[x][visited] = graph[x][0]
+        return graph[x][0]
     
-    if dp[x][visited] != INF:
+    if dp[x][visited] != -1:
         return dp[x][visited]
     
-    for i in range(1, n):
-        if not graph[x][i]:
-            continue
-        if visited & (1<<i):
-            continue
+    min_cost = INF
+    for i in range(n):
+        # if not visited i node yet && graph != 0
+        if not visited & (1 << i) and graph[x][i] != 0:
+            min_cost = min(min_cost, graph[x][i] + recursion(i, visited | (1 << i)))
+    
+    dp[x][visited] = min_cost
+    return min_cost
 
-        dp[x][visited] = min(dp[x][visited], dfs(i, visited | (1<<i)) + graph[x][i])
-    return dp[x][visited]
+n = int(input().rstrip())
+dp = [[-1 for _ in range(1 << n)] for _ in range(n)]
+graph = []
+for _ in range(n):
+    graph.append(list(map(int, input().split())))
 
-graph = [list(map(int, input().split())) for _ in range(n)]
-
-print(dfs(0, 1))
+print(recursion(0, 1))
