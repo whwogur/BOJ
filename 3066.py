bb4b5c6f856b8905e75c79a1d29a596ce3b72e5a
whwogur <cjh.lux@gmail.com>
2023-03-27 19:38:59 +0900

    브리징 시그널

---
+++ 3066.py
@@ -0,0 +1,13 @@
+import sys, bisect
+input = sys.stdin.readline
+
+for _ in range(int(input().rstrip())):
+    n = int(input().rstrip())
+    arr = [int(input().rstrip()) for _ in range(n)]
+    lis = [0]
+    for i in arr:
+        if i > lis[-1]:
+            lis.append(i)
+        else:
+            lis[bisect.bisect_left(lis,i)] = i
+    print(len(lis) - 1)
