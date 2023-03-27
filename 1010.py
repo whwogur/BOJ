9adad75a290dbf5263cea91e317c42e6907747ca
whwogur <cjh.lux@gmail.com>
2023-03-27 16:19:46 +0900

    다리 놓기

---
+++ 1010.py
@@ -0,0 +1,18 @@
+# 1010 다리 놓기
+import sys
+input = sys.stdin.readline
+
+for _ in range(int(input().rstrip())):
+    n, m = map(int, input().split())
+    dp = [[0 for _ in range(m + 1)] for _ in range(n+1)]
+    for i in range(1, n + 1):
+        for j in range(1, m + 1):
+            if i == 1:
+                dp[i][j] = j
+                continue
+            if i == j:
+                dp[i][j] = 1
+            else:
+                if j > i:
+                    dp[i][j] = dp[i][j - 1] + dp[i - 1][j - 1]
+    print(dp[n][m])
