df4f602210eee9d353eca54cd9e2374936889618
whwogur <cjh.lux@gmail.com>
2023-03-28 17:23:25 +0900

    전깃줄

--- 2565.py
+++ 2565.py
@@ -18,11 +18,11 @@ dic = {}
 n = int(input().rstrip())
 for _ in range(n):
     a, b = map(int, input().split())
-    dic[b] = a
-temp = sorted(dic)
+    dic[b] = a # b 전봇대 : a 전봇대
+temp = sorted(dic) # 키값(b전봇대)을 sorted list로
 for i in temp:
-    cables.append(dic.get(i))
-for i in cables:
+    cables.append(dic.get(i)) # cables = [4, 2, 6, 7, 9, 1, 3, 10]
+for i in cables: # cables 에서 LIS 길이 찾기
     if i > lis[-1]:
         lis.append(i)
     else:
