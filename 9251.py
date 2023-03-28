235a2dc8ba58de63b73978b0eb95341d774d49b1
whwogur <cjh.lux@gmail.com>
2023-03-28 21:31:24 +0900

    LCS

--- 9251.py
+++ 9251.py
@@ -11,20 +11,13 @@ m = len(str1)
 n = len(str2)
 
 def LCS_dp(str1, str2, m, n):
-    table = []
-
-    # m행 n열 0으로 초기화된 table 생성
-    for i in range(m+1):
-        line = []
-        for j in range(n+1):
-            line.append(0)
-        table.append(line)
+    table = [[0 for _ in range(m + 1)] for _ in range(n + 1)]
 
     for i in range(1, m+1):
         for j in range(1, n+1):
             if str1[i-1] == str2[j-1]: # 공통부분에 포함되기 때문에 LCS에도 추가
                 table[i][j] = table[i-1][j-1] + 1
-            else: # LSC에 포함되지 않으므로, 이전까지의 LCS와 동일하게 유지
+            else: # LCS에 포함되지 않으므로, 이전까지의 LCS와 동일하게 유지
                 table[i][j] = max(table[i-1][j], table[i][j-1])
 
     return table
