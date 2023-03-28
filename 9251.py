9d5c4174cc14784bd4515ad12292aa67b9b37805
whwogur <cjh.lux@gmail.com>
2023-03-28 20:32:36 +0900

    LCS

--- 9251.py
+++ 9251.py
@@ -1,17 +1,33 @@
 # 9251 LCS
+# 일치할 경우 dp[i][j] = dp[i-1][j-1] + 1
+# 불일치 할 경우 dp[i][j] = Max(dp[i-1][j], dp[i][j-1])
 import sys
 input = sys.stdin.readline
-MAXLEN = 1001
 
-string1 = '' + input().strip()
-string2 = '' + input().strip()
-dp = [[0] * len(string2) for _ in range(len(string1))]
+str1 = list(input().rstrip())
+str2 = list(input().rstrip())
 
-for i in range(1, len(string1)):
-    for j in range(1, len(string1)):
-        if string1[i] == string2[j]:
-            dp[i][j] = dp[i - 1][j - 1] + 1
-        else:
-            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+m = len(str1)
+n = len(str2)
 
-print(dp[-1][-1])
+def LCS_dp(str1, str2, m, n):
+    table = []
+
+    # m행 n열 0으로 초기화된 table 생성
+    for i in range(m+1):
+        line = []
+        for j in range(n+1):
+            line.append(0)
+        table.append(line)
+
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            if str1[i-1] == str2[j-1]: # 공통부분에 포함되기 때문에 LCS에도 추가
+                table[i][j] = table[i-1][j-1] + 1
+            else: # LSC에 포함되지 않으므로, 이전까지의 LCS와 동일하게 유지
+                table[i][j] = max(table[i-1][j], table[i][j-1])
+
+    return table
+
+result = LCS_dp(str1, str2, m, n)
+print(result[m][n])
