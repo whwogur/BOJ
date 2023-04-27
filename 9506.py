d0c4bd78b114d6690321fc2b329be130d7bd95ab
whwogur <cjh.lux@gmail.com>
2023-04-27 21:46:46 +0900

    약수들의 합

---
+++ 9506.py
@@ -0,0 +1,13 @@
+# 9506 약수들의 합
+while True:
+    n = int(input())
+    if n == -1: # 입력 값이 -1이면 반복문 종료
+        break;
+    arr = []
+    for i in range(1, n):
+        if n % i == 0:
+            arr.append(i)
+    if sum(arr) == n:
+        print(n, " = ", " + ".join(str(i) for i in arr), sep="")
+    else:
+        print(n, "is NOT perfect.")
