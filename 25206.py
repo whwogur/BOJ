c1fe7c851a2e1f6ab2b0520a9feb251afc2efcd7
whwogur <cjh.lux@gmail.com>
2023-04-30 15:20:12 +0900

    너의 평점은

---
+++ 25206.py
@@ -0,0 +1,14 @@
+# 25206 너의 평점은
+grade = ['A+', 'A0', 'B+', 'B0', 'C+', 'C0', 'D+', 'D0', 'F']
+score = [4.5, 4.0, 3.5, 3.0, 2.5, 2.0, 1.5, 1.0, 0]
+
+total = 0
+result = 0
+for _ in range(20) :
+    s, p, g = input().split()
+    p = float(p)
+    if g != 'P' :
+        total += p
+        result += p * score[grade.index(g)]
+
+print('%.6f' % (result / total))
