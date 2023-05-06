fd27c36a3eded6fce66288fc45cd69cbbb9dc8c1
whwogur <cjh.lux@gmail.com>
2023-05-06 18:04:48 +0900

    어린왕자

---
+++ 1004.py
@@ -0,0 +1,11 @@
+# 1004 어린 왕자
+for _ in range(int(input())):
+    cnt = 0
+    x1, y1, x2, y2 = map(int, input().split())
+    for i in range(int(input())):
+        cx, cy, cr = map(int, input().split())
+        dist1 = (x1 - cx) ** 2 + (y1 - cy) ** 2
+        dist2 = (x2 - cx) ** 2 + (y2 - cy) ** 2
+        if (dist1 < cr**2 and dist2 > cr**2) or ( dist1 > cr**2 and dist2 < cr**2):
+            cnt += 1
+    print(cnt)
