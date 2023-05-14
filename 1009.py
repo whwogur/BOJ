db0b97bf8ac4d9a9be4dc52a8cfe3dad51ca93d3
whwogur <cjh.lux@gmail.com>
2023-05-14 17:44:44 +0900

    분산처리

---
+++ 1009.py
@@ -0,0 +1,20 @@
+# 1009 분산처리
+for _ in range(int(input())):
+    a, b = map(int, input().split())
+    a %= 10
+    if a == 0:
+        print(10)
+    elif a in (1,5,6):
+        print(a)
+    elif a in (4, 9):
+        b %= 2
+        if b:
+            print(a)
+        else:
+            print((a * a) % 10)
+    else:
+        b %= 4
+        if not b:
+            print((a**4) % 10 % 10 % 10)
+        else:
+            print((a**b) % 10 % 10 % 10)
